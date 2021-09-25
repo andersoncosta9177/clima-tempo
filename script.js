693dcaafef0329c8f84aeec4a4e7fdd2f41b1eb9
andersoncosta9177 <anderson_costa_miguel@hotmail.com>
2021-09-25 19:02:14 -0300

    app de clima em tempo real

---
+++ script.js
@@ -0,0 +1,60 @@
+document.querySelector('.busca').addEventListener('submit', async (event) => {
+    event.preventDefault()
+
+    let input = document.querySelector('#searchInput').value
+
+    if (input !== '') {
+        clearInfo()
+        showWarning('carregando...')
+
+        let url = `https://api.openweathermap.org/data/2.5/weather?q=${encodeURI(input)}&appid=5fd716f13d747c8c58167a01f8fadf40&units=metric&lang=pt_br`
+
+        let results = await fetch(url)
+        let json = await results.json()
+
+        console.log(json);
+
+        if (json.cod === 200)  {
+            showInfo({
+                name: json.name,
+                country: json.sys.country,
+                temp: json.main.temp,
+                tempIcon:json.weather[0].icon,
+                windSpeed: json.wind.speed,
+                windAngle: json.wind.deg,
+
+            });
+        }else{
+            clearInfo()
+            showWarning("Não encontramos essa localização.");
+
+        }
+    }else{
+        clearInfo()
+    }
+});
+
+
+function showInfo(json){
+ showWarning('')
+ document.querySelector('.resultado').style.display = 'block'
+
+   document.querySelector('.titulo').innerHTML = `${json.name}, ${json.country}`
+   document.querySelector('.tempInfo').innerHTML = `${json.temp} <sup>°C</sup>`;
+   document.querySelector('.ventoInfo').innerHTML =  `${json.windSpeed} <span>km/h</span>`
+
+document.querySelector('.temp img').setAttribute('src', `http://openweathermap.org/img/wn/${json.tempIcon}@2x.png`);
+document.querySelector('.ventoPonto').style.transform =`rotate(${json.windAngle-90}deg)`
+
+
+}
+
+function clearInfo(){
+    showWarning('');
+    document.querySelector('.resultado').style.display = 'none'
+}
+
+
+function showWarning(msg) {
+    document.querySelector('.aviso').innerHTML = msg
+}
